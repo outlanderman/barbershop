0b980f34662f3db89b801fd1251ccff3dbfa730a
outlanderman <rinaz.kashapov@yandex.ru>
2021-10-20 22:57:24 +0300

    add overlay

--- js/map.js
+++ js/map.js
@@ -1,20 +1,24 @@
 let mapOpen = document.querySelector(".js-open-map");
 let mapPopup = document.querySelector(".modal-content-map");
 let mapClose = mapPopup.querySelector(".modal-content-close");
+let mapOverlay = document.querySelector(".modal-overlay");
 
 
 mapOpen.addEventListener("click", function (event) {
 	event.preventDefault();
 	mapPopup.classList.add("modal-content-show");
+	mapOverlay.classList.add("modal-content-show");
 });
 mapClose.addEventListener("click", function (event) {
 	event.preventDefault();
 	mapPopup.classList.remove("modal-content-show");
+	mapOverlay.classList.remove("modal-content-show");
 });
 window.addEventListener("keydown", function (KeyboardEvent) {
 	if (KeyboardEvent.code === 'Escape') {
 		if (mapPopup.classList.contains("modal-content-show")) {
 			mapPopup.classList.remove("modal-content-show");
+			mapOverlay.classList.remove("modal-content-show");
 		}
 	}
 });
